1b8aefe443887deb709f35f7a07743f9597b8507
oroce <robert@oroszi.net>
2014-04-09 20:53:10 +0200

    added flatten and map load metrics

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 var godot = require("godot");
-
+var Flatten = require( "./lib/flatten" );
+godot.reactor.register( "flatten", Flatten );
 godot.createServer({
   type: "tcp",
   reactors: [
     godot.reactor()
       .on( "reactor:error", console.error.bind(console, "reactor:error" ) )
+      .map(function( data ){
+        if( data.meta  && data.meta.plugin === "load" ){
+          return [ "shortterm", "midterm", "longterm" ].map(function( type ){
+            var clone = godot.common.clone( data );
+            clone.metric = data.meta[ type ];
+            clone.service = [ clone.service, type ].join( "/" );
+            return clone;
+          });
+        }
+        return data;
+      })
+      .flatten()
       .movingAverage({
         duration: 10*1000,
         type: "simple"
--- client.js
+++ client.js
@@ -1,6 +1,6 @@
 var godot = require("godot");
 var producer = godot.producer({
-  ttl: 1000,
+  ttl: 10000/3,
   metric: 1
 });
 var client = godot.createClient({
@@ -11,7 +11,7 @@ var client = godot.createClient({
 });
 var i = 0;
 producer.produce = function(){
-  i = i < 9 ? i+1 : 1;
+  /*i = i < 9 ? i+1 : 1;
   this.emit('data', {
     host:        this.values.host,
     service:     "foo/bar/barfoo",
@@ -21,6 +21,17 @@ producer.produce = function(){
     tags:        this.values.tags,
     metric:      i,
     ttl:         this.values.ttl
+  });*/
+  this.emit('data', {
+    host:        this.values.host,
+    service:     "foo/bar/barfoo",
+    state:       this.values.state,
+    time:        Date.now(),
+    description: this.values.description,
+    tags:        this.values.tags,
+    metric:      this.values.metric,
+    ttl:         this.values.ttl,
+    meta:        {"@version":"1", "@timestamp":"2014-04-08T22:30:03.724Z","host":"bp01.purposeindustries.co","plugin":"load","collectd_type":"load","shortterm":7.58,"midterm":7.51,"longterm":7.6,"type":"collectd","is_it_cloned_st":"true","tags":["load-splitted"],"service":"production/collectd/load/%{type_instance}"}
   });
 }
 client.connect(1337);
